fd95015dc2bc838bb77168ea7adae74ac54eb35b
AlexLopEx03 <164388886+AlexLopEx03@users.noreply.github.com>
2025-08-13 23:21:08 +0100

    Create uninstaller.ps1

---
+++ uninstaller.ps1
@@ -0,0 +1,30 @@
+Write-Host "Running onion-chat Windows uninstaller"
+
+$localAppPath = "$env:USERPROFILE\AppData\Local\Onion-chat"
+
+try{
+    rmdir "$localAppPath" -Recurse -Force -ErrorAction Stop
+}catch{
+    Write-Error "Error while tried to remove the app folder at $localAppPath" -ForegroundColor Red
+    return
+}
+
+try{
+    $userPath = [Environment]::GetEnvironmentVariable("Path", "User")
+    $variables = $userPath -split ';'
+
+    $newPath = ""
+    for($i = 0; $i -lt $variables.Length; $i++){
+        if($variables[$i] -ne $localAppPath -and $variables[$i] -ne ""){
+            if($newPath -ne ""){
+                $newPath += ";"
+            }
+            $newPath += $variables[$i]
+        }
+    }
+    [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
+}catch{
+    Write-Error "Error while tried to remove environment variable" -ForegroundColor Red
+}
+
+Write-Host "onion-chat successfully uninstalled" -ForegroundColor Green
