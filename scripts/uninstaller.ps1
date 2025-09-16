33af6c2e2643606ce722a10f61725f49fcf36ca4
AlexLopEx03 <164388886+AlexLopEx03@users.noreply.github.com>
2025-09-16 02:01:06 +0100

    Create uninstaller.ps1

---
+++ scripts/uninstaller.ps1
@@ -0,0 +1,33 @@
+Write-Host ""
+
+Write-Host "Running onion-chat Windows uninstaller..."
+
+$localAppPath = "$env:USERPROFILE\AppData\Local\Onion-chat"
+
+try{
+    rmdir "$localAppPath" -Recurse -Force -ErrorAction Stop
+}catch{
+    Write-Error "Error while tried to remove the app folder at $localAppPath"
+    return
+}
+
+try{
+    $userPath = [Environment]::GetEnvironmentVariable("Path", "User")
+    $variables = $userPath -split ';'
+
+    $newPath = ""
+    for($i = 0; $i -lt $variables.Length; $i++){
+        if($variables[$i] -ne $localAppPath -and $variables[$i] -ne ""){
+            if($newPath -ne ""){
+                $newPath += ";"
+            }
+            $newPath += $variables[$i]
+        }
+    }
+    [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
+}catch{
+    Write-Error "Error while tried to remove environment variable"
+    return
+}
+
+Write-Host "onion-chat successfully uninstalled"
